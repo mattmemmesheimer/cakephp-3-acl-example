dcb18e5fd6a72ac5fd4af5e22b10bcdc857c8c06
mattmemmesheimer <m.memmesheimer@gmail.com>
2015-08-14 18:26:44 -0500

    Loading the ACL plugin.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -181,6 +181,7 @@ Request::addDetector('tablet', function ($request) {
  */
 
 Plugin::load('Migrations');
+Plugin::load('Acl', ['bootstrap' => true]);
 
 // Only try to load DebugKit in development mode
 // Debug Kit should not be installed on a production system
